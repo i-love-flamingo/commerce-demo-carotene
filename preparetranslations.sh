0aec693fe51343d01dc0192835fe397555c51cde
Joachim Adomeit <joachim.adomeit@aoe.com>
2019-06-24 15:04:37 +0200

    temporary quickfix to get translation building up again, needs replacement when https://github.com/nicksnyder/go-i18n is ready

--- preparetranslations.sh
+++ preparetranslations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Preparing merged file for common side.. (main)"
-goi18n merge -sourceLanguage en-gb -format yaml -outdir translations/merged translations/src/*.json
+bin/goi18n merge -sourceLanguage en-gb -format yaml -outdir translations/merged translations/src/*.json
 
-#goi18n merge -sourceLanguage en-gb -format yaml -outdir translations/merged translations/src/*.json translations/translated/*.yaml
+#bin/goi18n merge -sourceLanguage en-gb -format yaml -outdir translations/merged translations/src/*.json translations/translated/*.yaml
 
