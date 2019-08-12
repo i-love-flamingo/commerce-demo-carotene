5061d8df2e46db9bd1243498aa10bec718aba8f1
Carsten Dietrich <carsten.dietrich@aoe.com>
2019-08-12 19:55:13 +0200

    Update preparetranslations.sh to use go run

--- preparetranslations.sh
+++ preparetranslations.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 echo "Preparing merged file for common side.. (main)"
-bin/goi18n merge -sourceLanguage en-gb -format yaml -outdir translations/merged translations/src/*.json
-
-#bin/goi18n merge -sourceLanguage en-gb -format yaml -outdir translations/merged translations/src/*.json translations/translated/*.yaml
-
+go run github.com/nicksnyder/go-i18n/goi18n merge -sourceLanguage en-gb -format yaml -outdir translations/merged translations/src/*.json
