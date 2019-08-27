b41e595ab059d497e35554ef94ed38e75209fae4
Daniel Pötzinger <daniel.poetzinger@aoe.com>
2019-08-27 09:57:21 +0200

    Improve product page to support variants

---
+++ frontend/src/page/product/product.js
@@ -0,0 +1,9 @@
+
+jQuery(function () {
+    jQuery("#variant-selector").change(function () {
+        let url = jQuery("#variant-selector").children("option:selected").data('url')
+        if (url != undefined && url != "") {
+            window.location.href = url
+        }
+    });
+});
